2a3fa345e12ae39256009d96a7585b7f2003d583
martinoremonje <m.monjeorellana@gmail.com>
2025-03-30 13:45:18 -0300

    arreglos menores

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from '../assets/icono-juan.jpg'
+import logo from '../assets/icono-juan.jpg';
 
 const NavBar = () => {
   return (
     <div className="bg-green-100 p-4 flex items-center justify-between w-full">
-      
-      <span className='flex'><img src={logo} alt="Logo" className="h-12 w-12 rounded-full" />Vigesta</span>
+      <div className="flex items-center">
+        <img src={logo} alt="Logo" className="h-12 w-12 rounded-full" />
+        <span className="ml-2 text-lg font-semibold flex items-center h-12">Vigesta</span>
+      </div>
       <nav>
         <a href="/" className="mx-2 text-gray-800">Inicio</a>
         <a href="/acerca" className="mx-2 text-gray-800">Acerca</a>
